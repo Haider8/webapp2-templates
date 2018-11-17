735e00f037b7fa0599efc05d2f6f8a814c342461
Haider Ali <haider.lee23@gmail.com>
2018-11-17 23:38:34 +0530

    add shopping list feature

--- app.py
+++ app.py
@@ -5,9 +5,28 @@ form_html = """
 <form>
 <h2>Add a food</h2>
 <input type="text" name="food">
-<input type="hidden" name="food" value="eggs">
+%s
 <button>Add</button>
 </form>
+"""
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
+"""
+
+item_html = """
+<li>
+%s
+</li>
+"""
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping list</h2>
+<ul>
+%s
+</ul
 """
 
 class Handler(webapp2.RequestHandler):
@@ -16,6 +35,21 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
     def get(self):
-        self.write(form_html)
+        output = form_html
+        output_hidden = ""
+
+        items = self.request.get_all("food")  # list of all the food parameters
+        output_items = ""
+        if items:
+            for item in items:
+                output_hidden += hidden_html % item
+                output_items += item_html % item
+
+            output_shopping = shopping_list_html % output_items
+            output += output_shopping
+
+        output = output % output_hidden
+
+        self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage), ], debug=True)

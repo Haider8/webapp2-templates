600b0356eb36b3ccc16ae2e491a31f31b5bc3c1d
Haider Ali <haider.lee23@gmail.com>
2018-11-18 00:38:41 +0530

    render templates and use jinja2

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import os
+import jinja2
 import webapp2
+
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')  # templates directory added in our current dir.
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
 form_html = """
 <form>
@@ -33,23 +39,32 @@ class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render(self, template, **kw):  # **kw are extra parameters
+        self.response.out.write(self.render_str(template, **kw))
+
+    def render_str(self, template, **params):  # **params are also extra parameters
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
 class MainPage(Handler):
     def get(self):
-        output = form_html
-        output_hidden = ""
+        self.render("shopping_list.html", name=self.request.get('name'))
+        # output = form_html
+        # output_hidden = ""
+        #
+        # items = self.request.get_all("food")  # list of all the food parameters
+        # output_items = ""
+        # if items:
+        #     for item in items:
+        #         output_hidden += hidden_html % item
+        #         output_items += item_html % item
+        #
+        #     output_shopping = shopping_list_html % output_items
+        #     output += output_shopping
+        #
+        # output = output % output_hidden
+        #
+        # self.write(output)
 
-        items = self.request.get_all("food")  # list of all the food parameters
-        output_items = ""
-        if items:
-            for item in items:
-                output_hidden += hidden_html % item
-                output_items += item_html % item
-
-            output_shopping = shopping_list_html % output_items
-            output += output_shopping
-
-        output = output % output_hidden
-
-        self.write(output)
 
 app = webapp2.WSGIApplication([('/', MainPage), ], debug=True)
